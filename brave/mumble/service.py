676146e57f6b90c933c6eb514328f7d73252c236
Alice Bevan-McGregor <alice@gothcandy.com>
2014-01-29 02:08:46 -0500

    Sample code authenticator.

--- brave/mumble/service.py
+++ brave/mumble/service.py
@@ -25,6 +25,88 @@ AUTH_FAIL = (-1, None, None)
 NO_INFO = (False, {})
 
 
+
+
+
+
+
+class ServerAuthenticatorI(Murmur.ServerUpdatingAuthenticator):
+    def authenticate(self, name, pw, certlist, certhash, strong, current=None):
+      print certhash, strong
+      for cert in certlist:
+        cert = X509.load_cert_der_string(cert)
+        print cert.get_subject(), "issued by", cert.get_issuer()
+      groups = ("GroupA", "GroupB");
+      if (name == "One"):
+        if (pw == "Magic"):
+          return (1, "One", groups)
+        else:
+          return (-1, None, None)
+      elif (name == "Two"):
+        if (pw == "Mushroom"):
+          return (2, "twO", groups)
+        else:
+          return (-1, None, None)
+      elif (name == "White Space"):
+        if (pw == "Space White"):
+          return (3, "White Space", groups)
+        else:
+          return (-1, None, None)
+      elif (name == "Fail"):
+        time.sleep(6)
+      return (-2, None, None)
+
+    def getInfo(self, id, current=None):
+      print "getInfo ", id
+      name = self.idToName(id);
+      if (name == None):
+        return (False, {})
+      map = {}
+      map[Murmur.UserInfo.UserName]=name
+      return (True, map)
+
+    def nameToId(self, name, current=None):
+      if (name == "One"):
+        return 1
+      elif (name == "Twoer"):
+        return 2
+      else:
+        return -2;
+
+    def idToName(self, id, current=None):
+      if (id == 1):
+        return "One"
+      elif (id == 2):
+        return "Two"
+      else:
+        return None
+
+    def idToTexture(self, id, current=None):
+      print "idToTexture", id
+      return open("../icons/mumble.osx.png").read();
+
+    # The expanded methods from UpdatingAuthenticator. We only implement a subset for this example, but
+    # a valid implementation has to define all of them
+    def registerUser(self, name, current=None):
+      print "Someone tried to register " + name[Murmur.UserInfo.UserName]
+      return -2
+
+    def unregisterUser(self, id, current=None):
+      print "Unregister ", id
+      return -2
+
+    def getRegistration(self, id, current=None):
+      return (-2, None, None)
+    
+    def setInfo(self, id, info, current=None):
+      print "Set", id, info
+      return -1
+
+
+
+
+
+
 class MumbleAuthenticator(Murmur.ServerAuthenticator):
     """MongoDB-backed Mumble authentication agent.
     
@@ -263,7 +345,7 @@ class MumbleAuthenticatorApp(Ice.Application):
         metacbprx = adapter.addWithUUID(MumbleMetaCallback(self))
         self.metacb = Murmur.MetaCallbackPrx.uncheckedCast(metacbprx)
         
-        authprx = adapter.addWithUUID(MumbleAuthenticator())
+        authprx = adapter.addWithUUID(ServerAuthenticatorI())
         self.auth = Murmur.ServerUpdatingAuthenticatorPrx.uncheckedCast(authprx)
         
         return self.attachCallbacks()
@@ -326,7 +408,7 @@ def main(secret):
     """
     
     PYTHONPATH=/usr/local/lib/python2.7/site-packages paster shell
-    from brave.mumble.service import main
+    from brave.mumble.service import main; main('')
     
     """
     log.info("Ice initializing.")
