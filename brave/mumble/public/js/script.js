fe701923f7df22822bd78ee826bb6e774bc060ca
Matthew Glinski <matthewglinski@gmail.com>
2014-01-31 14:36:10 -0500

    Changed Selection Method for username

--- brave/mumble/public/js/script.js
+++ brave/mumble/public/js/script.js
@@ -24,6 +24,26 @@
 }());
 
 
+// TExt Selection jQuery Plugin
+jQuery.fn.selectText = function(){
+    var doc = document
+        , element = this[0]
+        , range, selection
+    ;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
+
+
 
 
 $(function(){
